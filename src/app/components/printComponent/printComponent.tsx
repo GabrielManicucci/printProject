e4f39762e1aaaaf29c522fdef7b66fff6cc246aa
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2024-06-05 11:17:34 -0300

    feature: add barcode

---
+++ src/app/components/printComponent/printComponent.tsx
@@ -0,0 +1,26 @@
+// components/LabelComponent.js
+import React, { forwardRef } from "react";
+
+export default function PrintComponent() {
+  return <div>teste</div>;
+}
+
+// const LabelComponent = forwardRef((props, ref) => {
+//   return (
+//     <div
+//       ref={ref}
+//       s
+//       style={{
+//         width: "50mm",
+//         height: "100mm",
+//         padding: "5mm",
+//         border: "1px solid black",
+//       }}
+//     >
+//       <h1>Etiqueta Vertical</h1>
+//       <p>Conteúdo da etiqueta</p>
+//     </div>
+//   );
+// });
+
+// export default LabelComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,119 @@
 "use client";
 import Image from "next/image";
-import { useRef } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 import { useReactToPrint, ReactToPrint } from "react-to-print";
+import Barcode from "react-barcode";
 
-import {
-  Br,
-  Cut,
-  Line,
-  Printer,
-  Text,
-  Row,
-  render,
-} from "react-thermal-printer";
+// import {
+//   Br,
+//   Cut,
+//   Line,
+//   Printer,
+//   Text,
+//   Row,
+//   render,
+//   Barcode,
+// } from "react-thermal-printer";
 
-export default function Home() {
-  const componentToPrint = useRef(null);
+export default function Main() {
+  // const receipt = (
+  //   <Printer
+  //     className="w-[5cm]"
+  //     type="epson"
+  //     width={42}
+  //     characterSet="pc860_portuguese"
+  //   >
+  //     <Text size={{ width: 2, height: 2 }}>Produto Teste</Text>
+  //     <Text bold={true}>Produto</Text>
+  //     <Br />
+  //     <Line />
+  //     <Row left="lore ipsum" right="863" />
+  //     <Row left="lore ipsum" right="8,637" />
+  //     <Line />
+  //     <Row left="lore ipsum" right="11,000" />
+  //     <Barcode type="UPC-A" content="111111111111" />
+  //     <Barcode type="CODE39" content="A000$" />
+  //     <Barcode align="center" type="UPC-A" content="111111111111" />
+  //     <Text>Valor: R$ 1200</Text>
+  //     <Row left="Status: ok" right="- 500" />
+  //     <Br />
+  //     <Line />
+  //     <Br />
+  //     <Text align="center">Wifi: some-wifi / PW: 123123</Text>
+  //     <Cut />
+  //   </Printer>
+  // );
+
+  const [isPrinting, setIsPrinting] = useState(false);
+  const printRef = useRef(null);
+
+  // We store the resolve Promise being used in `onBeforeGetContent` here
+  const promiseResolveRef = useRef<(() => void) | null>(null);
+
+  // We watch for the state to change here, and for the Promise resolve to be available
+  useEffect(() => {
+    if (isPrinting) {
+      // Resolves the Promise, letting `react-to-print` know that the DOM updates are completed
+      console.log("ok");
+    }
+  }, [isPrinting]);
 
   const handlePrint = useReactToPrint({
-    content: () => componentToPrint.current,
+    documentTitle: "Document for print",
+    pageStyle: `
+      @page {
+        size: 225mm 50mm;
+        margin: 0;
+      }
+      @media print {
+        body {
+        margin: 0;
+        }
+      }
+  `,
+    content: () => printRef.current,
+    onBeforeGetContent: async () => {
+      try {
+        // const data: Uint8Array = await render(receipt);
+        setIsPrinting(true);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onAfterPrint: () => {
+      // Reset the Promise resolve so we can print again
+      promiseResolveRef.current = null;
+      setIsPrinting(false);
+    },
   });
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="font-medium text-2xl">Impressão</h1>
+    <div className="flex min-h-screen flex-col items-center p-24 gap-3">
+      {/* <div className="bg-stone-300 w-[5cm] h-[22.5cm]" ref={printRef}>
+        {receipt}
+      </div> */}
+      <div ref={printRef} className="flex bg-stone-300 w-[225mm] h-[50mm] p-4">
+        <Barcode
+          value="123456789012"
+          format="CODE128"
+          width={1}
+          height={30}
+          displayValue={true}
+          fontOptions=""
+          fontSize={12}
+          margin={10}
+          lineColor="black"
+        />
+        <h1>Etiqueta vertical Teste</h1>
+        <p>conteúdo teste</p>
+      </div>
 
       <button
         className="mt-12 px-6 py-4 bg-slate-600 text-slate-100 rounded-md"
         onClick={handlePrint}
       >
-        Imprimir
+        Imprimir!
       </button>
-    </main>
+    </div>
   );
 }
